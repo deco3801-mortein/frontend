48754f0efa2285b2524d8ca7bc878af0f1373130
Amelia Caddie <amelia.caddie@gmail.com>
2024-09-22 22:48:35 +1000

    Add Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import "./Login.css";
 function Login() {
     return (
         <div className="login-page">
-            <h1>Login Page</h1>
+            <div className="left-balancer"></div>
+            <div className="login-main">
+                <h1 className="login-title">VibeGrow</h1>
+                <div className="user-login">
+                    <div className="account-div">
+                        <label htmlFor="account">Account:</label>
+                        <input type="text" id="account" name="account" />
+                    </div>
+                    <div className="password-div">
+                        <label htmlFor="password">Password:</label>
+                        <input type="text" id="password" name="password" />
+                    </div>
+                </div>
+                <div className="button-div">
+                    <Link to="/">
+                        <button className="login-button">Login</button>
+                    </Link>
+                </div>
+            </div>
+            <div className="slash-container">
+                <div className="login-slash"></div>
+            </div>
         </div>
     );
 }
