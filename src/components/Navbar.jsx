553e4a9e5228ee6d9baf471839c6c2e34e7b3791
Amelia Caddie <amelia.caddie@gmail.com>
2024-09-25 07:47:06 +0000

    Add prop handling of navigation to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,30 @@
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom"; // Import useNavigate for navigation
 // Navigation header - used for both detail and plants pages
 // (with different props values)
 function Navbar(props) {
+    const navigate = useNavigate(); // Initialize the navigation hook
+
+    const handleSearchClick = () => {
+        navigate("/search"); // Navigate to the search page when the icon is clicked
+    };
+
+    const handleMenuClick = () => {
+        navigate("/login"); // Navigate to the login page when the icon is clicked
+    };
+
+    const handleBackClick = () => {
+        navigate(-1);
+    }
+
+    const clickLeftFunction = props.left == "Back" ? handleBackClick : handleMenuClick;
+    const guideButton = <h2 className="menu-icon" onClick={handleSearchClick} style={{cursor:'pointer'}}>&#128269;</h2>;
+
     return (
         <nav className="navbar">
-            <h2>{props.left}</h2>
-            <h1>{props.title}</h1>
-            <h2>Guide</h2>
+            {props.left == "Back" ? <h2 className="back-icon" onClick={clickLeftFunction} style={{cursor:'pointer'}}>&lt;</h2> : <h2 className="menu-icon" onClick={clickLeftFunction} style={{cursor:'pointer'}}>&#9776;</h2>}
+            <h1 className="navbar-title">{props.title}</h1>
+            {props.showGuide ? guideButton : <div></div>};
         </nav>
     );
 }
@@ -14,6 +32,7 @@ function Navbar(props) {
 Navbar.propTypes = {
     left: PropTypes.string,
     title: PropTypes.number,
+    showGuide: PropTypes.bool
 };
 
 export default Navbar;
