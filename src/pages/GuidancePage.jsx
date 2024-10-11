d8cfae894bc58b90ac3e44d2337a30d4dfec5921
Amelia Caddie <amelia.caddie@gmail.com>
2024-10-11 00:09:22 +0000

    Add guidance page

--- src/pages/GuidancePage.jsx
+++ src/pages/GuidancePage.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./GuidancePage.css";
-import Header from "../components/Header";
-import plantImagePath from "../assets/img/plant.gif";
-import pestImagePath from "../assets/img/pest.gif";
+import { useNavigate } from 'react-router-dom';
+import './GuidancePage.css';
+import Header from '../components/Header';
+import plantImagePath from '../assets/img/plant.gif';
+import pestImagePath from '../assets/img/pest.gif';
+
 
 const GuidancePage = () => {
     const navigate = useNavigate();
@@ -12,12 +12,12 @@ const GuidancePage = () => {
         <div className="garden-guidance-page">
             <Header left="Back" title="Garden Guidance" showGuide={false} />
             <div className="icon-container">
-                <div className="icon-circle" onClick={() => navigate("/search")}>
+                <div className="icon-circle"  onClick={() => navigate('/search')}>
                     <img src={plantImagePath} alt="Plant" className="custom-icon" />
                 </div>
-                <p onClick={() => navigate("/search")}>Plant</p>
+                <p  onClick={() => navigate('/search')}>Plant</p>
             </div>
-            <div className="icon-container" onClick={() => navigate("/pest-search")}>
+            <div className="icon-container" onClick={() => navigate('/pest-search')}>
                 <div className="icon-circle">
                     <img src={pestImagePath} alt="Pest" className="custom-icon" />
                 </div>
