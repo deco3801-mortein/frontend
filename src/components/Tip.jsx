a04e8726a9bdea59a908d933efe03e777b5cbf07
Amelia Caddie <amelia.caddie@gmail.com>
2024-10-14 05:40:29 +0000

    Prettier

--- src/components/Tip.jsx
+++ src/components/Tip.jsx
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 function Tip({ content, onClose }) {
     return (
         <div className="tip-content">
-                <p>{content}</p>
-                <button onClick={onClose}>Close</button>
+            <p>{content}</p>
+            <button onClick={onClose}>Close</button>
         </div>
     );
 }
