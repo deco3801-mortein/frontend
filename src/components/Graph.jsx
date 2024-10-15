53287f46fde20b0d6ea88d493ae0249f4cd90788
Amelia Caddie <amelia.caddie@gmail.com>
2024-10-15 12:16:05 +0000

    Graph code adjustments

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -3,39 +3,50 @@ import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tool
 // Function for graph component
 function Graph({ data }) {
     // get latest reading date information
-    const date = new Date(data[0].timestamp);
-    const year = date.getFullYear();
-    const dayOfMonth = date.getDate();
-    const month = date.getMonth();
+    let averages = [];
+    for (let i = 0; i < data.length; i = i + 10) {
+        const group = data.slice(i, i + 10);
+        let sum = 0;
+        for (let j = 0; j < group.length; j++) {
+            sum += group[j].sunlight;
+        }
+        averages.push(sum / group.length);
+    }
+    // const date = new Date(data[0].timestamp);
+    // const year = date.getUTCFullYear();
+    // const dayOfMonth = date.getUTCDate();
+    // const month = date.getUTCMonth();
 
-    // get all data from the same day as date variable
-    const dayData = data.filter((d) => {
-        const dataDate = new Date(d.timestamp);
-        return dataDate.getFullYear() == year && dataDate.getMonth() == month && dataDate.getDate() == dayOfMonth;
-    });
+    // // get all data from the same day as date variable
+    // const dayData = data.filter((d) => {
+    //     const dataDate = new Date(d.timestamp);
+    //     return dataDate.getUTCFullYear() == year && dataDate.getUTCMonth() == month && dataDate.getUTCDate() == dayOfMonth;
+    // });
 
-    console.log(dayData);
-    dayData.sort((a,b) => {
-        return new Date(a.timestamp) - new Date(b.timestamp);
-    });
-    console.log(dayData);
+    // console.log(dayData);
+    // dayData.sort((a,b) => {
+    //     return new Date(a.timestamp) - new Date(b.timestamp);
+    // });
+    // console.log(dayData);
 
-    const timeData = dayData.map((d) => {
-        const dataDate = new Date(d.timestamp);
-        return {timestamp: `${dataDate.getHours()}:${dataDate.getMinutes()}:${dataDate.getSeconds()}`,
-                seconds: dataDate.getTime(),
-                sunlight: d.sunlight
-                }
-    })
-    // check whether all hours have been iterated through
-    // while (currentHour < 24) {
-    //     for (let i = 0; i < dayData.length; i++) {
-    //         const dataDate = new Date(dayData[i].timestamp); // the date data for the current data point
-    //         // check if data hour is in the currentHour
-    //         if (dataDate.getHours() == currentHour) {
-    //             currentHourData.push(dayData[i])
-    //         }
+    // const timeData = dayData.map((d) => {
+    //     const dataDate = new Date(d.timestamp);
+    //     return {timestamp: `${dataDate.getUTCHours()}:${dataDate.getUTCMinutes()}:${dataDate.getUTCSeconds()}`,
+    //             seconds: dataDate.getTime(),
+    //             sunlight: d.sunlight
+    //             }
+    // })
+    // const currentDate = new Date(dayData[0].timestamp);
+    // console.log(currentDate);
+    // const currentMinute = currentDate.getUTCMinutes();
+    // console.log(currentMinute);
+    // for (let i = 0; i < dayData.length; i++) {
+    //     const dataDate = new Date(dayData[i].timestamp); // the date data for the current data point
+    //     // check if data hour is in the currentHour
+    //     if (dataDate.getUTCMinutes() == currentMinute) {
+    //         console.log("hello");
     //     }
+    // }
 
     //     // only store the sunlight data 
     //     const sunlightValues = currentHourData.map((d) => {
@@ -51,17 +62,26 @@ function Graph({ data }) {
     //     hours.push(currentHour);
     // }
     
-    console.log(timeData);
+    //console.log(timeData);
+    let index = -1;
+    const newFormat = averages.map((d) => {
+        index++;
+        return {
+            id: index,
+            sunlight: d
+        }
+    });
+
     return (
         <>
-        <h2>{`${dayOfMonth}-${month+1}-${year}`}</h2>
+            {/* <h2>{`${dayOfMonth}-${month+1}-${year}`}</h2> */}
             <ResponsiveContainer width="100%" aspect={3}>
-                {dayData && <LineChart data={timeData}>
-                    <XAxis dataKey="timestamp" />
-                    <YAxis dataKey="sunlight" />
-                    <Tooltip />
-                    <Line type="monotone" dataKey="sunlight" fill="#8884d8" />
-                </LineChart>}
+                    {newFormat && <LineChart data={newFormat}>
+                        <XAxis type="number" dataKey="id" />
+                        <YAxis dataKey="sunlight" />
+                        <Tooltip />
+                        <Line type="monotone" dataKey="sunlight" fill="#8884d8" />
+                    </LineChart>}
             </ResponsiveContainer>
         </>
     );
