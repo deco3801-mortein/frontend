d8a554dfb91dadf304883a17646e64142c25a8b8
Amelia Caddie <amelia.caddie@gmail.com>
2024-10-17 03:25:09 +0000

    Adjust comments and reorder imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Device } from "@deco3801-mortein/mortein-sdk/services.gen";
 import HomePage from "./pages/HomePage";
 import Detail from "./pages/Detail";
-import "./App.css";
-import "./pages/Detail.css";
 import SearchPage from "./pages/SearchPage";
 import SearchDetail from "./pages/SearchDetail";
 import Login from "./pages/Login";
 import PestPage from "./pages/PestPage";
 import GuidancePage from "./pages/GuidancePage";
 import PestDetail from "./pages/PestDetail";
-import { Device } from "@deco3801-mortein/mortein-sdk/services.gen";
+import "./App.css";
+import "./pages/Detail.css";
 
 function App() {
+    // stores whether the device exists in the server
     const [devicesNotFound, setDevicesNotFound] = useState(false);
 
     const [devices, setDevices] = useState(null);
 
+    // fetch all devices from the server
     useEffect(() => {
         Device.getDevice()
             .then((data) => {
@@ -27,29 +29,6 @@ function App() {
                 setDevicesNotFound(true);
             });
     }, []);
-
-    // Function to toggle vibration
-
-    // function toggleVibration(event) {
-    //     let newDevices = [];
-    //     for (let i = 0; i < userData.devices.length; i++) {
-    //         let device = userData.devices[i];
-    //         if (device.id === event.target.value) {
-    //             device = {
-    //                 ...device,
-    //                 data: {
-    //                     ...device.data,
-    //                     vibration: !device.data.vibration,
-    //                 },
-    //             };
-    //         }
-    //         newDevices.push(device);
-    //     }
-    //     setUserData((prevData) => ({
-    //         ...prevData,
-    //         devices: newDevices,
-    //     }));
-    // }
 
     return (
         <Router>
@@ -68,7 +47,7 @@ function App() {
                     path="/detail/:id"
                     element={
                         devices && (
-                            <Detail /> //toggleVibration={toggleVibration} />
+                            <Detail />
                         )
                     }
                 />
